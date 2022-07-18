0a2d3032f8dc8dce91e34f0ad79c4811d8e63344
Konstantin Dobler <konstantin.j.dobler@gmail.com>
2022-07-18 14:52:33 +0200

    trying out docker solutions

--- no_command_specified.py
+++ no_command_specified.py
@@ -1,10 +1,7 @@
-
-
 print(
     "Empty docker run command. docker run received no command arguments / script to run. Please specifiy like this: docker run <options> <my-command>. This is a custom warning."
 )
 
-
 import torch
 
-torch.cuda_is_available()
+print("torch availability check: ", torch.cuda.is_available())
